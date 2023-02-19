605812ee577065d852f587461eec793b341c8955
Pazy0g <renot.ibep56@icloud.com>
2023-02-19 18:16:41 +0100

    t

--- index.js
+++ index.js
@@ -189,8 +189,9 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
-
-    console.log("collision2");
+    player.health -= 20;
+    document.querySelector('#playerHealth').style.width = player.health + "%";
+   
   }
 }
 animate();

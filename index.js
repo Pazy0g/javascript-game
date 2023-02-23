dfa3ae667cd3aafe2a7088e3d1f6e817502a7dfd
Pazy0g <renot.ibep56@icloud.com>
2023-02-23 20:05:05 +0100

    Added background to the scene

--- index.js
+++ index.js
@@ -10,71 +10,19 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 //* Adding gravity to prevent models from falling off the screen
 const gravity = 0.7;
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: 'assets/background.png'
 
+})
 //? Might add a custom property to prevent the model from jumping off the screen
 
 //! Creating model objects for both players and enemies
-//* Mother class for models
-class Sprite {
-  constructor({ position, velocity, color = "red", offset }) {
-    this.position = position;
-    this.velocity = velocity;
-    this.width = 50;
-    this.height = 150;
-    this.lastKey;
-    this.attackBox = {
-      position: {
-        x: this.position.x,
-        y: this.position.y,
-      },
-      offset,
-      width: 100,
-      height: 50,
-    };
-    this.color = color;
-    this.isAttacking;
-    this.health = 100;
-  }
-
-  draw() {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-    // attack box
-    if (this.isAttacking) {
-      c.fillStyle = "blue";
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height
-      );
-    }
-  }
-
-  update() {
-    this.draw();
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
-
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0;
-    } else this.velocity.y += gravity;
-  }
-
-  attack() {
-    this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100);
-  }
-}
-
-//! Creating player object with default values for {Velocity and Position}
-const player = new Sprite({
+//! Creating player object with default values for {Velocity, Position and offset}
+const player = new Fighter({
   position: {
     x: 0,
     y: 0,
@@ -90,7 +38,7 @@ const player = new Sprite({
 });
 
 //! Creating enemy objects with default values for {Velocity and Position}
-const enemy = new Sprite({
+const enemy = new Fighter({
   position: {
     x: 400,
     y: 100,
@@ -133,11 +81,11 @@ const keys = {
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
+    rectangle2.position.x &&
     rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
+    rectangle2.position.x + rectangle2.width &&
     rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
+    rectangle2.position.y &&
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   );
 }
@@ -176,6 +124,7 @@ function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
+  background.update();
   player.update();
   enemy.update();
 
